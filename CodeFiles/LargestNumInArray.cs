4c89711adbccb132b91203534ee15dfa1586a731
Sarthak Pathak <sarthak1@umbc.edu>
2021-06-03 22:20:10 -0400

    added two practise question from hired .com

---
+++ CodeFiles/LargestNumInArray.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructureAndAlgo
+{
+	public class LargestNumInArray
+	{
+		private long largetstNum(long[] numbers)
+		{
+			if (numbers.Length <= 0) return 0;
+			Array.Sort(numbers);
+			var len = numbers.Length;
+			var largest = numbers[len - 1];
+
+			return largest;
+		}
+	}
+}
