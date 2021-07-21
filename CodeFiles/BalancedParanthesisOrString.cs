247f3b884bc09e57c908532a986313067e1a3fe3
Sarthak Pathak <sarthak1@umbc.edu>
2021-07-20 23:04:03 -0400

    added balanced paranthesis code

--- CodeFiles/BalancedParanthesisOrString.cs
+++ CodeFiles/BalancedParanthesisOrString.cs
@@ -10,7 +10,9 @@ namespace DataStructureAndAlgo
 		{
 			var input = "([])(){}(())()()";
 			var input2 = "(({{[[([)])]]}}))";
-			Console.WriteLine(BalancedBrackets(input2));	
+			var input3 = "((AB)(BC)(()";
+			Console.WriteLine(BalancedBrackets(input2));
+			Console.WriteLine(BalancedParanthesis(input3));
 		}
 		public bool BalancedBrackets(string str)
 		{
@@ -18,6 +20,7 @@ namespace DataStructureAndAlgo
 			var closingBrackets = ")]}";
 			Dictionary<string, string> bracketsMap = new Dictionary<string, string>();
 			Stack<string> bracketStack = new Stack<string>();
+
 
 			bracketsMap.Add(")", "(");
 			bracketsMap.Add("]", "[");
@@ -39,6 +42,49 @@ namespace DataStructureAndAlgo
 			}
 			if (bracketStack.Count > 0) return false;
 			return true;
+		}
+
+		public string BalancedParanthesis(string str) //"((AB)(BC)(()";
+		{
+			StringBuilder finalStr = new StringBuilder();
+			finalStr.Append(str);
+			var openingBrackets = "([{";
+			var closingBrackets = ")]}";
+			Dictionary<string, string> bracketsMap = new Dictionary<string, string>();
+			Stack<Dictionary<int, string>> bStack = new Stack<Dictionary<int, string>>();
+
+			bracketsMap.Add(")", "(");
+			bracketsMap.Add("]", "[");
+			bracketsMap.Add("}", "{");
+
+			for (int i = 0; i < str.Length; i++)
+			{
+				var currVal = str[i].ToString();
+				var idxValPair = new Dictionary<int, string>();
+				if (openingBrackets.Contains(currVal))
+				{
+					idxValPair.Add(i, currVal);
+					bStack.Push(idxValPair);
+				}
+				else if(closingBrackets.Contains(currVal))
+				{
+					if(bStack.Peek().ContainsValue(bracketsMap[currVal]))
+					{
+						bStack.Pop();
+					}
+				}
+			}
+			if (bStack.Count > 0)
+			{
+				foreach (var item in bStack)
+				{
+					foreach (var key in item.Keys)
+					{
+						finalStr.Remove(key, 1);
+					}
+				}
+			}
+			return finalStr.ToString();
 		}
 	}
 }
