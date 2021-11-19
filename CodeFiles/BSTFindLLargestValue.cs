ac6732e509b4f482af38e8c4b06017eb664ad102
Sarthak Pathak <sarthak1@umbc.edu>
2021-11-18 21:40:53 -0500

    BST find kth largest value by reverse inorder traversal

--- CodeFiles/BSTFindLLargestValue.cs
+++ CodeFiles/BSTFindLLargestValue.cs
@@ -4,6 +4,16 @@ using System.Text;
 
 namespace DataStructureAndAlgo
 {
+    public class TreeInfo
+    {
+        public int NumberOfNodesVisited { get; set; }
+        public int NodeLatestValue { get; set; }
+        public TreeInfo(int nodesVisited, int latestVal)
+        {
+            NumberOfNodesVisited = nodesVisited;
+            NodeLatestValue = latestVal;    
+        }
+    }
     internal class BSTFindKLargestValue
     {
         public BSTFindKLargestValue()
@@ -11,6 +21,7 @@ namespace DataStructureAndAlgo
             var array = new int[] { 1, 2, 3, 5, 5, 15, 20, 17, 22 };
             var bst = new BSTMinHeight().constructMinHeightBSTV3(array, 0, array.Length - 1);
             Console.WriteLine(FindKthLargestValueInBst_ViaInOrder(bst, 3));
+            Console.WriteLine(FindKthLargestValueInBst_ViaReverseInOrder(bst,3));
         }
         public int FindKthLargestValueInBst_ViaInOrder(BST tree, int k)
         {
@@ -27,6 +38,28 @@ namespace DataStructureAndAlgo
                 inOrder(tree.left, array);
                 array.Add(tree.value);
                 inOrder(tree.right, array);
+            }
+        }
+
+        public int FindKthLargestValueInBst_ViaReverseInOrder(BST tree, int k)
+        {
+            TreeInfo treeInfo = new TreeInfo(0, -1);
+
+            reverseInOrder(tree, k, treeInfo);
+ 
+            return treeInfo.NodeLatestValue;
+
+        }
+        private void reverseInOrder(BST tree, int k, TreeInfo treeInfo)
+        {
+            if (tree == null || treeInfo.NumberOfNodesVisited >= k) return;
+
+            reverseInOrder(tree.right, k, treeInfo);
+            if(treeInfo.NumberOfNodesVisited < k)
+            {
+                treeInfo.NumberOfNodesVisited += 1;
+                treeInfo.NodeLatestValue = tree.value;
+                reverseInOrder(tree.left, k,treeInfo);
             }
         }
     }
